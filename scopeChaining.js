4dffbbb0792e74a3265d211399fcd388c36d085f
Siddharth <33184198+iamSiddharth08@users.noreply.github.com>
2020-08-08 13:55:54 +0530

    Defines the scope

---
+++ scopeChaining.js
@@ -0,0 +1,11 @@
+var str = "This is the string"
+
+console.log(str)
+
+// The fuction will ask the variable str from the global
+function theStr()
+{
+    console.log("The string is ", str)
+}
+
+theStr()
